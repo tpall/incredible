8224e76c33925592d957561885e2d7d3360b732c
Taavi Päll <tapa741@gmail.com>
2017-10-08 22:52:19 +0300

    comment out checks

--- _deploy.sh
+++ _deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-set -e
-
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
-git config --global user.email "tapa741@gmail.com"
-git config --global user.name "Taavi Päll"
+# set -e
+# 
+# [ -z "${GITHUB_PAT}" ] && exit 0
+# [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+# 
+# git config --global user.email "tapa741@gmail.com"
+# git config --global user.name "Taavi Päll"
 
 echo "Starting rsync"
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs
