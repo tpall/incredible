ee91d2c2510b70f37f12975785a878f3e8779e08
Taavi Päll <tapa741@gmail.com>
2017-10-08 20:57:53 +0300

    deploy script

---
+++ _deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+git config --global user.email "tapa741@gmail.com"
+git config --global user.name "Taavi Päll"
+
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs/
