2ff18711122f6713058d578bf7b800eedaafc6a4
Taavi Päll <tapa741@gmail.com>
2017-10-12 00:49:20 +0300

    ssh -i

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 
 echo "Starting rsync"
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs
+rsync -r -e "ssh -i ~/.ssh/authorized_keys" --delete-after --quiet $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs
 
 exit 0
