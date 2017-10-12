bee45a0ad91f4037d85c90956f4f70c18d80e203
Taavi Päll <tapa741@gmail.com>
2017-10-12 09:32:44 +0300

    removed exec flag

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting rsync"
-rsync -r -e --delete-after --quiet $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs
+rsync -qr --delete-after $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs
