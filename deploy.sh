18ac13ccb00ea12eafbc806d4f85d826eee4c6ba
Taavi Päll <tapa741@gmail.com>
2017-10-12 15:52:27 +0300

    fix path, ssh still not working

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ ssh -vv $DEPLOY_USER@$DEPLOY_HOST
 
 echo "Starting rsync"
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/public $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/public/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
