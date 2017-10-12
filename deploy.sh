e030719797c7a50c8c404409c3f16e8c5e7b0bac
Taavi Päll <tapa741@gmail.com>
2017-10-12 09:19:24 +0300

    now?

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-echo "ssh-add sucess?"
-echo $?
-echo "run ls"
-ls
-echo "list keyss"
-ls -l ~/.ssh
-# echo "Starting rsync"
-# rsync -r -e --delete-after --quiet $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs
-# exit 0
-
+echo "Starting rsync"
+rsync -r -e --delete-after --quiet $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs
