cc53653aae0f1d7a992347eaee5e268158dfd66e
Taavi Päll <tapa741@gmail.com>
2017-10-12 09:42:25 +0300

    what

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 echo "Starting rsync"
 ls
 ls $TRAVIS_BUILD_DIR/public
-rsync -qr --delete-after --quiet $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs
