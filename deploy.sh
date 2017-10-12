de6001ee0e41d127ec5b329754cb5ce0a289f469
Taavi Päll <tapa741@gmail.com>
2017-10-12 21:23:37 +0300

    almost there

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-echo "test ssh"
-ssh -vT $DEPLOY_USER@$DEPLOY_HOST
-
 echo "Starting rsync"
 
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/public/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
