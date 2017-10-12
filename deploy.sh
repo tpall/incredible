33b320accbf901d60624a99185da53a817e01032
Taavi Päll <tapa741@gmail.com>
2017-10-12 09:11:55 +0300

    test ssh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+echo "ssh-add sucess?"
+echo $?
+echo "run ls"
+ls
+echo "list keyss"
+ls -l ~/.ssh
+# echo "Starting rsync"
+# rsync -r -e --delete-after --quiet $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs
+# exit 0
 
-echo "Starting rsync"
-rsync -r -e "ssh -i ~/.ssh/authorized_keys" --delete-after --quiet $TRAVIS_BUILD_DIR/public virt64551@credibleinterval.ee:~/domeenid/www.credibleinterval.ee/htdocs
-
-exit 0
